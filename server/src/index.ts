55fb86e54bb6d5ec3a2f812c3e58b03f2c08397c
yash-devop <yashkamble.dev@gmail.com>
2025-06-28 18:35:51 +0530

    package: cookie-parser

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,14 @@ import express from "express";
 import CredentialsRouter from "./routes/auth/credentials/credentials.route";
 import OauthRouter from "./routes/auth/oauth/oauth.route";
 import { ErrorHandler } from "./lib/errors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = 8000;
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/api/v1", CredentialsRouter, OauthRouter);
 app.use(ErrorHandler);
