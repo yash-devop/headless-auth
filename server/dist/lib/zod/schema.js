ba9f4ed6754bde29a8dcf90ff931cc83733115c5
yash-devop <yashkamble.dev@gmail.com>
2025-06-28 16:28:14 +0530

    Stop tracking dist folder

--- server/dist/lib/zod/schema.js
+++
@@ -1,9 +0,0 @@
-// src/lib/zod/schema.ts
-import { z } from "zod";
-var SignUpSchema = z.object({
-  email: z.string({ message: "Email is required" }).email("Invalid Email").min(1).transform((email) => email.toLowerCase()),
-  password: z.string({ message: "Password is required" }).min(8, "Password must be of 8 characters").max(30, "Password must be less than 30 characters")
-});
-export {
-  SignUpSchema
-};
