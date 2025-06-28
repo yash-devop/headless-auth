650b1fe331dfe6cd8274857a2f50e46568a434ae
yash-devop <yashkamble.dev@gmail.com>
2025-06-28 18:37:47 +0530

    testing authenticate middleware

--- server/src/routes/auth/oauth/oauth.route.ts
+++ server/src/routes/auth/oauth/oauth.route.ts
@@ -2,11 +2,16 @@ import {
   googleAuthHandler,
   initialiseGoogleAuth,
 } from "@/controllers/oauth.controller";
+import { authenticate } from "@/middlewares/authenticate";
 import express from "express";
 
 const router = express.Router();
 
 router.get("/auth/google", initialiseGoogleAuth);
 router.get("/auth/google/callback", googleAuthHandler);
+router.get("/me", authenticate, async (req, res) => {
+  const user = req.user;
+  res.json({ user });
+});
 
 export default router;
